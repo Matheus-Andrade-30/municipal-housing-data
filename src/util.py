121030bd839995e21e07d00a12e6c1b19a9defe1
matheus <mtandradeg28@gmail.com>
2025-08-12 12:55:33 -0300

    feat: criação da util dados_limpos_Situacao

--- src/util.py
+++ src/util.py
@@ -31,3 +31,39 @@ def dados_limpos_Total(df: pd.DataFrame) -> pd.DataFrame:
         df[col] = pd.to_numeric(df[col], errors="coerce")
 
     return df
+
+def dados_limpos_Situacao(df: pd.DataFrame) -> pd.DataFrame:
+    df.rename(columns={
+    'Domicílio': 'domicilio',
+    'Regiao' : 'regiao',
+    'Domicílios Rústicos' : 'domicilios_rusticos',
+    'Domicílios Improvisados' : 'domicilios_improvisados',
+    'Habitação Precária' : 'habitacao_precaria',
+    'Cômodos' : 'comodos',
+    'Unidades conviventes déficit' : 'unidades_conviventes_deficit',
+    'Coabitação' : 'coabitacao',
+    'Ônus' : 'onus',
+    'Déficit Habitacional (Habitação Precária + Coabitação + Ônus)' : 'deficit_original'
+    }, inplace=True)
+
+    colunas_excluir = ["regiao", "domicilio"]
+
+    colunas_converter = [col for col in df.columns if col not in colunas_excluir]
+
+    for col in colunas_converter:
+        df[col] = (
+            df[col]
+            .astype(str)
+            .str.replace('.', '', regex=False)
+            .str.replace(',', '.', regex=False)
+        )
+    df[col] = pd.to_numeric(df[col], errors='coerce')
+    df['onus'] = df['onus'].fillna(0)
+
+    df['deficit_total'] = df['habitacao_precaria'] + df['coabitacao'] + df['onus']
+    df.drop(columns=['N', 'deficit_original'], inplace=True)
+
+    df_regioes = df[df['regiao'] != 'Brasil'].copy()
+    df_brasil = df[df['regiao'] == 'Brasil'].copy()
+
+    return df_regioes, df_brasil
